52eb049251f95d499a7c4a27313362e58f4e9fa9
bulatkhabib <khabibullin23@mail.ru>
2020-09-28 15:40:14 +0300

    second commit

---
+++ sql/V2__teachers_create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE teamdb.teachers (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`, `user_id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
